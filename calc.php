e32975dd73a968e74d67336f140529e51c85d16f
MonubiJustin <monubijustin@gmail.com>
2026-02-10 12:01:31 +0300

    looping in php

--- calc.php
+++ calc.php
@@ -1,31 +1,39 @@
 <?php
 
-$num1 = (int)readline("Enter num1: ");
-$operation = readline("Enter (+, -, *, /): ");
-$num2 = (int)readline("Enter num2: ");
+$flag = true;
 
 
-switch ($operation){
-    case "+":
-        $result = $num1 + $num2;
-        echo "$num1 + $num2 = $result\n";
-        break;
-    case "-":
-        $result = $num1 - $num2;
-        echo "$num1 - $num2 = $result\n";
-        break;
-    case "*":
-        $result = $num1 * $num2;
-        echo "$num1 * $num2 = $result\n";
-        break;
-    case "/":
-        if ($num2 != 0){
-        $result = $num1 / $num2;
-        echo "$num1 / $num2 = $result\n";
-        }
-        else
-            echo "Zero Division Error\n";
-        break;
-    default:
-        echo "Invalid Operation\n";
+while ($flag) {
+    $num1 = (float)readline("Enter num1: ");
+    $operation = readline("Enter (+, -, *, /): ");
+    $num2 = (float)readline("Enter num2: ");
+
+
+    switch ($operation) {
+        case "+":
+            $result = $num1 + $num2;
+            echo "$num1 + $num2 = $result\n";
+            break;
+        case "-":
+            $result = $num1 - $num2;
+            echo "$num1 - $num2 = $result\n";
+            break;
+        case "*":
+            $result = $num1 * $num2;
+            echo "$num1 * $num2 = $result\n";
+            break;
+        case "/":
+            if ($num2 != 0) {
+                $result = $num1 / $num2;
+                echo "$num1 / $num2 = $result\n";
+            } else
+                echo "Zero Division Error\n";
+            break;
+        default:
+            echo "Invalid Operation\n";
+    }
+
+    $option = readline("enter 1 to continue, otherwise press anything else to quit:  \n");
+    $flag = $option == "1" ? true : false;
+    echo "\n";
 }
